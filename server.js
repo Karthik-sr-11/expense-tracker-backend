f7d5395b364d1f08005f8e02cff650759fb6968a
Karthik-sr-11 <karthik.lance18@gmail.com>
2025-11-30 00:04:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // CORS – allow ALL (important for Netlify + Render)
-const cors = require("cors");
 
 app.use(cors({
     origin: "*",
@@ -43,6 +42,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
 
 
 
